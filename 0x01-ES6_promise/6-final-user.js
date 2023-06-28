9191fc24016810ceb57253455c69cd8d8a0f484f
Arinze Nwutobo <tim.arinze@outlook.com>
2023-06-28 11:20:17 +0100

    task

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const results = [];
+      values.forEach((value) => {
+        results.push(value.status);
+        if (value.status === 'rejected') {
+          results.push(value.reason.message);
+        } else {
+          results.push(value.value.body);
+        }
+      });
+      return results;
+    });
 }
