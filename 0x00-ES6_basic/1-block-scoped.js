4a7dcedf0c8a3741ecaee54a230cb158ce622d7f
Arinze Nwutobo <tim.arinze@outlook.com>
2023-06-29 14:16:14 +0100

    task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,14 @@
 export default function taskBlock(trueOrFalse) {
+  // eslint-disable-next-line no-unused-vars
   const task = false;
+  // eslint-disable-next-line no-unused-vars
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
