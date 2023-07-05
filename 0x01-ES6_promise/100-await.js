2dbb32ea11b78e62688b7c7d030385f992ecf655
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-05 23:24:56 +0100

    task

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,23 +1,31 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  async function creating() {
-    const create = await createUser();
-    if (create) {
-      return create;
-    }
-    return null;
+//   async function creating() {
+//     const create = await createUser();
+//     if (create) {
+//       return create;
+//     }
+//     return null;
+//   }
+//   async function uploading() {
+//     const upload = await uploadPhoto();
+//     if (upload) {
+//       return upload;
+//     }
+//     return null;
+//   }
+  const creating = await createUser();
+  const uploading = await uploadPhoto();
+  if (creating && uploading) {
+    const userDetails = {
+      photo: uploading,
+      user: creating,
+    };
+    return userDetails;
   }
-  async function uploading() {
-    const upload = await uploadPhoto();
-    if (upload) {
-      return upload;
-    }
-    return null;
-  }
-  const userDetails = {
-    photo: await uploading(),
-    user: await creating(),
+  return {
+    photo: null,
+    user: null,
   };
-  return userDetails;
 }
