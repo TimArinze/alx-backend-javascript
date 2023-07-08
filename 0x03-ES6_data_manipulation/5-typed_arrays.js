f539766f6f1f035be45a9ba65cb10346c743bc8f
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-08 22:17:23 +0100

    task

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
-  try {
+  if (position > length) {
+    throw Error('Position outside range');
+  } else {
     int8view[position] = value;
     const view = new DataView(buffer);
     return view;
-  } catch (error) {
-    console.error('Position outside range', error);
-    throw error;
   }
 }
