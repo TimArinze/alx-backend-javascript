8ef391b089724679c3acf26fc3bddee97a0450d5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-07-09 15:48:14 +0100

    task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-    if (!startString || typeof startString !== 'string') {
-        return '';
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  const newSet = new Set();
+  set.forEach((value) => {
+    if (value && value.startsWith(startString)) {
+      newSet.add(value.slice(startString.length));
     }
-    const newSet = new Set();
-    set.forEach((value) => {
-        if (value && value.startsWith(startString)) {
-        newSet.add(value.slice(startString.length));
-        }
-    });
-    return Array.from(newSet).join('-');
+  });
+  return Array.from(newSet).join('-');
 }
