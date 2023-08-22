3cb39a985f2858b4e6374546ac81c6b1229b835a
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-22 19:17:21 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 const readline = require('readline');
 
-const message = 'Welcome to Holberton School, what is your name?\n';
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const message = 'Welcome to Holberton School, what is your name?\n';
 rl.question(message, (name) => {
   console.log(`Your name is: ${name}`);
 });
