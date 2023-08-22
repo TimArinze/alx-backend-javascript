aba0867b12c392583d1b037aea6c2b8ea2816dc5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-22 19:38:32 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,11 +3,11 @@
 const message = 'Welcome to Holberton School, what is your name?\n';
 process.stdout.write(message);
 
-process.stdin.on('data', data => {
-	const input = data.toString().trim()
-	process.stdout.write(`Your name is: ${input}\n`)
-})
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+});
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n')
+  process.stdout.write('This important software is now closing\n');
 });
