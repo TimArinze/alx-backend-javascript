975f455e0c0d2a769b8b2ed98c57275dd1c4a488
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-24 00:29:25 +0100

    task

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const reader = require('./3-read_file_async');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    reader(process.argv[2]).then((data) => {
+      res.end(data);
+    }).catch((err) => {
+      res.end(err.message);
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
