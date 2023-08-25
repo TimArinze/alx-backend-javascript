2478a8a2b005e93ffd0e4861e60e16d5ceaf5f23
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-25 01:13:05 +0100

    task

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,15 @@
+const mocha = require("mocha")
+const { expect } = require("chai")
+const calculateNumber = require("./1-calcul.js")
+
+describe("calculateNumber", () => {
+    it("calculates", () => {
+        expect(calculateNumber("SUM", 1.5, 3.7)).to.equal(6)
+        expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4)
+        expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2)
+        expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error")
+        expect(calculateNumber("SUM", 0.5, 0)).to.equal(1)
+        expect(calculateNumber("DIVIDE", 0, 0)).to.equal("Error")
+        expect(calculateNumber("SUBTRACT", 0.5, 0.2)).to.equal(1)
+    });
+});
