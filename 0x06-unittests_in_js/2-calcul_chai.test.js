77dfe7e68800251288705fef30d97230a0cb75c2
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-25 01:16:44 +0100

    task

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,6 +1,6 @@
 const mocha = require("mocha")
-const { expect } = require("chai")
-const calculateNumber = require("./1-calcul.js")
+const expect = require("chai").expect
+const calculateNumber = require("./2-calcul_chai.js")
 
 describe("calculateNumber", () => {
     it("calculates", () => {
