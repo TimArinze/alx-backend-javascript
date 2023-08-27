bb98055f0d060595c237f33119c0603f33e7ed06
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-27 16:00:21 +0100

    test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,17 +5,12 @@ const expect = chai.expect;
 const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./3-payment.js');
 
-const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-
-
 describe('sendPaymentRequestToApi', () => {
 	it('validate the usage of the Utils function', () => {
-		const spy = sinon.spy(console, 'log');
+		const spy = sinon.spy(Utils, 'calculateNumber');
 		sendPaymentRequestToApi(100, 20);
-		expect(spy.calledWith('The total is: 120')).to.be.true;
-		expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+		expect(spy.calledWith("SUM", 100, 20)).to.be.true;
 		spy.restore();
-		calculateNumberSpy.restore();
 	}
 	);
 });
