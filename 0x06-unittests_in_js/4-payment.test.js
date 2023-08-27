2c2bd535fc8723b6175cbaa600b82f2fbd5c2725
Arinze Nwutobo <tim.arinze@outlook.com>
2023-08-28 00:38:47 +0100

    task

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+// payment test
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+        it('validate the usage of the Utils function', () => {
+                const stub = sinon.stub(Utils, 'calculateNumber');
+		const spy = sinon.spy(console, 'log')
+		stub.withArgs("SUM", 100, 20).returns(10)
+		sendPaymentRequestToApi(100, 20);
+                expect(stub.calledWith("SUM", 100, 20)).to.be.true;
+		expect(spy.calledWith("The total is:", 10))
+                stub.restore();
+        }
+        );
+});
