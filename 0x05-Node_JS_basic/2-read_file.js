8b1513b299c3700e93989c69abb78ef7db904ed5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-11-04 19:54:07 +0100

    task

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,26 +4,25 @@ const countStudents = (path) => {
   fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw Error('Cannot load the database');
-    } else {
-      const rows = data.split('\n').slice(1);
-      const students = [];
-      rows.forEach((row) => {
-        const student = row.split(',');
-        students.push(student);
-      });
-      const CS = [];
-      const SWE = [];
-      students.forEach((student) => {
-        if (student[3] === 'CS') {
-          CS.push(student[0]);
-        } else if (student[3] === 'SWE') {
-          SWE.push(student[0]);
-        }
-      });
-      console.log(`Number of students: ${CS.length + SWE.length}`);
-      console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
-      console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
     }
+    const rows = data.split('\n').slice(1);
+    const students = [];
+    rows.forEach((row) => {
+      const student = row.split(',');
+      students.push(student);
+    });
+    const CS = [];
+    const SWE = [];
+    students.forEach((student) => {
+      if (student[3] === 'CS') {
+        CS.push(student[0]);
+      } else if (student[3] === 'SWE') {
+        SWE.push(student[0]);
+      }
+    });
+    console.log(`Number of students: ${CS.length + SWE.length}`);
+    console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`);
   });
 };
 
