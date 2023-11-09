7566caf9362c4148007d95a59ed498f053f98c37
Arinze Nwutobo <tim.arinze@outlook.com>
2023-11-09 11:23:13 +0100

    task

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const reader = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const path = process.argv[2];
+  reader(path)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`List to port: ${port}`);
+});
+
+module.exports = app;
