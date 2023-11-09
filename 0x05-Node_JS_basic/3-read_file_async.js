9b0b09286ea08dbb8911907541293f0c71eca2a3
Arinze Nwutobo <tim.arinze@outlook.com>
2023-11-09 01:08:02 +0100

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,27 +8,29 @@ function countStudents(path) {
     fs.readFile(path, 'utf8', (error, data) => {
       if (error) {
         reject(Error('Cannot load the database'));
+      } else {
+        const rows = data.split('\n').slice(1);
+        const students = [];
+        rows.forEach((row) => {
+          const student = row.split(',');
+          students.push(student);
+        });
+        const CS = [];
+        const SWE = [];
+        students.forEach((student) => {
+          if (student[3] === 'CS') {
+            CS.push(student[0]);
+          } else if (student[3] === 'SWE') {
+            SWE.push(student[0]);
+          }
+        });
+        const totalNumberStudents = `Number of students: ${CS.length + SWE.length}\n`;
+        const csStudents = `Number of students in CS: ${CS.length}. List: ${CS.join(', ')}\n`;
+        const sweStudents = `Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`;
+        const output = totalNumberStudents + csStudents + sweStudents;
+        console.log(output);
+        resolve(output);
       }
-      const rows = data.split('\n').slice(1);
-      const students = [];
-      rows.forEach((row) => {
-        const student = row.split(',');
-        students.push(student);
-      });
-      const CS = [];
-      const SWE = [];
-      students.forEach((student) => {
-        if (student[3] === 'CS') {
-          CS.push(student[0]);
-        } else if (student[3] === 'SWE') {
-          SWE.push(student[0]);
-        }
-      });
-      const totalNumberStudents = `Number of students: ${CS.length + SWE.length}\n`;
-      const csStudents = `Number of students in CS: ${CS.length}. List: ${CS.join(', ')}\n`;
-      const sweStudents = `Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`;
-      const output = totalNumberStudents + csStudents + sweStudents;
-      resolve(console.log(output));
     });
   });
 }
